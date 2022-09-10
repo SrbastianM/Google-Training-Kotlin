442f043baec6adb448a22b6bfa3c7495b3297aac
Tenki <srodriguezdev.l@gmail.com>
2022-09-10 12:07:26 -0500

    New course in platzi about layouts and desing interfaces

--- ColorsMyView/app/src/main/java/com/example/colorsmyview/MainActivity.kt
+++ ColorsMyView/app/src/main/java/com/example/colorsmyview/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.colorsmyview
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
@@ -24,7 +27,6 @@ class MainActivity : AppCompatActivity()
             R.id.box_three_text -> view.setBackgroundColor(Color.CYAN)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.YELLOW)
-
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -36,15 +38,21 @@ class MainActivity : AppCompatActivity()
         val boxThree = findViewById<TextView>(R.id.box_three_text)
         val boxFour = findViewById<TextView>(R.id.box_four_text)
         val boxFive = findViewById<TextView>(R.id.box_five_text)
+        val imageTest = findViewById<ImageView>(R.id.image_one_test)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableList:
-                List<View> = listOf(boxOne, boxTwo, boxThree, boxFour, boxFive, rootConstraintLayout)
+                List<View> = listOf(boxOne, boxTwo, boxThree, boxFour, boxFive, rootConstraintLayout, imageTest)
 
         for (item in clickableList)
         {
             item.setOnClickListener { overColored(it) }
         }
+    }
+    private fun setOnClickListener(){
+        val invisibleImage : Boolean = true
+        val imageView = findViewById<ImageView>(R.id.image_one_test)
+        val buttomOne = findViewById<TextView>()
     }
 }
